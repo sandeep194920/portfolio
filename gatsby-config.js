bc3776456c10304b8c5c2ca235f759833c570725
sandeep194920 <sandeepamaranath@gmail.com>
2020-12-19 13:54:24 -0500

    built my portfolio site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 require(`dotenv`).config({
   path: `.env`,
-})
+});
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Sandeep Amarnath Portfolio`,
   },
   plugins: [
     {
@@ -35,13 +35,8 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
+            src: `/android-icon-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
             type: `image/png`,
           },
         ],
@@ -58,4 +53,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
